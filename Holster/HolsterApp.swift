716ba0e8c60818cd44719bfcb9f8a21a9ebd107d
quaquaquaqua <upascal@gmail.com>
2026-01-25 18:46:42 +0100

    Initial Commit

---
+++ Holster/HolsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HolsterApp.swift
+//  Holster
+//
+//  Created by Vibes on 1/25/26.
+//
+
+import SwiftUI
+
+@main
+struct HolsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
